2460bdf892a62c35c54475ab1241049335f5c211
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-31 16:21:03 +0530

    countwords.C
    count words

---
+++ countwords.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    char str;
+    int i, words;
+    printf("Enter any string: ");
+    gets(str);
+    i = 0;
+    words = 1;
+    while(str[i] != '\0')
+    {
+        if(str[i]==' ' || str[i]=='\n' || str[i]=='\t')
+        {
+            words++;
+        }
+
+        i++;
+    }
+
+    printf("Total number of words = %d", words);
+}
