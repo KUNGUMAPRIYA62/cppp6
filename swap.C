3ed59c623bafa2c61063d4f6b1f7402228b219fb
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-02 15:56:36 +0530

    swap.C
    swap of two numbers

---
+++ swap.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+printf("enter two numbers:");
+scanf("%d%d",&a,&b);
+{
+printf("Before swap");
+printf("value of a=%d,a);
+printf("value of b=%d,b);
+}
+swap(&a,&b);
+{
+printf("After swap");
+printf("value of a=%d,a);
+printf("value of b=%d",b);
+}
+}
