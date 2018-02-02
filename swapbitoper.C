80dc2e0dbb7ee2f534ae07a593f92b564deab57c
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-02 16:05:00 +0530

    swapbitoper.C
    swap bit wise operator

---
+++ swapbitoper.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+printf("enter two numbers:");
+scanf("%d%d",&a,&b);
+{
+printf("Before swap");
+printf("value of a=%d,a);
+printf("value of b=%d,b);
+}
+a=b;
+b=a;
+a=b;
+{
+printf("After swap");
+printf("value of a=%d,a);
+printf("value of b=%d",b);
+}
+}
