075ac055032f873d3af511bc1ff7ba12910bff9a
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-02-02 16:53:43 +0530

    ntimestring.C

---
+++ ntimestring.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[10];
+int i,k;
+printf("enter the string");
+scanf("%s",&string);
+printf("enter number of times");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+printf("%s",s);
+}
